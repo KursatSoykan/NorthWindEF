707ac0d83b1ebb19774fd1acbf4e46ef506210b3
KursatSoykan <162479099+KursatSoykan@users.noreply.github.com>
2024-03-12 19:07:55 +0300

    ilk pua-sh

--- NorthWindEF/NorthWindEF/Models/ORM/Category.cs
+++ NorthWindEF/NorthWindEF/Models/ORM/Category.cs
@@ -16,5 +16,6 @@ namespace NorthWindEF.Models.ORM
         public string CategoryName { get; set; }
         public string? Description { get; set; }
         public byte[]? Picture { get; set; }
+
     }
 }
